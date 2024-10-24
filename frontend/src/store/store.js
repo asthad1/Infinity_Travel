1c10b1c2e9e2f9b5514be405eac83222cd472d80
Winton Ebanks <83297039+Wint-1234@users.noreply.github.com>
2024-10-24 14:34:53 -0500

    Updated store to include saved searches

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,16 +1,17 @@
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import searchReducer from './searchSlice';
 import flightsReducer from './flightsSlice';
+import savedSearchesReducer from './savedSearchesSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     search: searchReducer,
     flights: flightsReducer,
+    savedSearches: savedSearchesReducer,
   },
-  devTools: process.env.NODE_ENV !== 'production', // Enable devTools only for non-production environments
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
