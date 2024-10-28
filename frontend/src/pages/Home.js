e227afc14b5682ce5820ad7cffa59ebcd23c6fbb
omatthew-tech <72327693+omatthew-tech@users.noreply.github.com>
2024-10-28 03:51:39 -0500

    forgot to change back text

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -26,7 +26,7 @@ function Home() {
           </div>
           {/* Right Column: Text */}
           <div className="col-md-6 text-center text-md-start">
-            <h1 className="display-4">testetststs to Infinity Travel</h1>
+            <h1 className="display-4">Welcome to Infinity Travel</h1>
             <p className="lead">
               Plan your next adventure with us and explore the world!
             </p>
