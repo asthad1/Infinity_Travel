053ecdd73fc914e83ceedd1dcdfeca9239aec315
omatthew-tech <72327693+omatthew-tech@users.noreply.github.com>
2024-11-20 18:02:20 -0500

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -132,6 +132,7 @@ function Navbar() {
                     )}
                   </a>
                   <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><Link className="dropdown-item" to="/addemails">Add Emails</Link></li>
                     <li><Link className="dropdown-item" to="/profile">Change Password</Link></li>
                     <li><Link className="dropdown-item" to="/my-favorites">My Favorites</Link></li>
                     <li><Link className="dropdown-item" to="/my-hotels">My Hotels</Link></li>
